177b59b2fcf9f4d3d8e57cd90f685fdf639a8348
zhengwx1998 <1798634552@qq.com>
2016-10-05 22:47:50 +0800

    Reference(MySQL/PBKDF2)
    Reference(MySQL/PBKDF2)

---
+++ PersonalWorkPlatform/PWP/Encryption/pbkdf2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWP.Encryption
+{
+    public class pbkdf2
+    {
+        public static void main(string[] passwordargs)
+        {
+            if (passwordargs.Length == 0)
+            {
+
+            }
+            else
+            {
+                string pwd1 = passwordargs[0];
+                //创建一个Byte数组用于存放随机数
+                byte[] salt1 = new byte[8];
+                using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+                {
+                    //使用随机数填充数组
+                    rngCsp.GetBytes(salt1);
+                }
+            }
+        }
+    }
+}
