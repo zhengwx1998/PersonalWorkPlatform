75caa4b5b0130ed9f29791fd37463105599bdfb5
zhengwx1998 <1798634552@qq.com>
2016-10-06 10:17:35 +0800

    Daily Update 1
    HMAC_SHA1 & PDKDF2

---
+++ PersonalWorkPlatform/PWP/Encryption/HMAC_SHA1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWP.Encryption
+{
+    class HMAC_SHA1
+    {
+        public static void Main(s)
+    }
+}
---
+++ PersonalWorkPlatform/PWP/Encryption/pbkdf2Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWP.Encryption
+{
+    public class pbkdf2Data
+    {
+        private string Key;
+        private string Salt;
+        public string key
+        {
+            set { Key = value; }
+            get { return Key; }
+        }
+        public string salt
+        {
+            set { Salt = value; }
+            get { return Salt; }
+        }
+    }
+}
